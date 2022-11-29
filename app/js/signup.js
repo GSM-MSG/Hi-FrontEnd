7a5f4c4a175a8a0431cdfae75ecf62176bc3dcf3
googoo81 <shw9052@naver.com>
2022-11-29 14:56:41 +0900

    focus error modify

--- app/js/signup.js
+++ app/js/signup.js
@@ -98,5 +98,3 @@ let num1 = document.querySelector(".num1").addEventListener("change", onChange);
 let num2 = document.querySelector(".num2").addEventListener("change", onChange);
 let num3 = document.querySelector(".num3").addEventListener("change", onChange);
 let num4 = document.querySelector(".num4").addEventListener("change", onChange);
-
-// numInput.addEventListener("change");/
