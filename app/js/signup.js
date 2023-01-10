257a103e2b00e3e87734d8d9a2db4cdbbbdd3ba9
googoo81 <shw9052@naver.com>
2023-01-10 21:07:30 +0900

    레이아웃 임시

--- app/js/signup.js
+++ app/js/signup.js
@@ -29,44 +29,44 @@ function GoToEnroll()
 }
 
 
-/* Password accord check */
-let target = document.getElementById("pw");
-let target2 = document.getElementById("pw2");
+    /* Password accord check */
+    let target = document.getElementById("pw");
+    let target2 = document.getElementById("pw2");
 
-function check_pw(){
- 
-    let pw = document.getElementById('pw').value;
-    let SC = ["!","@","#","$","%", "*"];
-    let check_SC = 0;
+    function check_pw(){
+    
+        let pw = document.getElementById('pw').value;
+        let SC = ["!","@","#","$","%", "*"];
+        let check_SC = 0;
 
-    if(pw.length < 8 || pw.length > 16){
-        window.alert('비밀번호는 8글자 이상, 16글자 이하만 이용 가능합니다.');
-        document.getElementById('pw').value='';
-    }
-    for(var i=0;i<SC.length;i++){
-        if(pw.indexOf(SC[i]) != -1){
-            check_SC = 1;
+        if(pw.length < 8 || pw.length > 16){
+            window.alert('비밀번호는 8글자 이상, 16글자 이하만 이용 가능합니다.');
+            document.getElementById('pw').value='';
+        }
+        for(var i=0;i<SC.length;i++){
+            if(pw.indexOf(SC[i]) != -1){
+                check_SC = 1;
+            }
+        }
+        if(check_SC == 0){
+            window.alert('!,@,#,$,%,*의 특수문자가 들어가 있지 않습니다.')
+            document.getElementById('pw').value='';
+        }
+        if(document.getElementById('pw').value !='' && document.getElementById('pw2').value!=''){
+            if(document.getElementById('pw').value==document.getElementById('pw2').value){
+                target.style.borderBottom = "3px solid black";
+                target2.style.borderBottom = "3px solid black";
+                document.getElementById('check').innerHTML='비밀번호가 일치합니다.'
+                document.getElementById('check').style.color='black';
+            }
+            else{
+                target.style.borderBottom = "3px solid red";
+                target2.style.borderBottom = "3px solid red";
+                document.getElementById('check').innerHTML='비밀번호가 일치하지 않습니다.';
+                document.getElementById('check').style.color='red';
+            }
         }
     }
-    if(check_SC == 0){
-        window.alert('!,@,#,$,%,*의 특수문자가 들어가 있지 않습니다.')
-        document.getElementById('pw').value='';
-    }
-    if(document.getElementById('pw').value !='' && document.getElementById('pw2').value!=''){
-        if(document.getElementById('pw').value==document.getElementById('pw2').value){
-            target.style.borderBottom = "3px solid black";
-            target2.style.borderBottom = "3px solid black";
-            document.getElementById('check').innerHTML='비밀번호가 일치합니다.'
-            document.getElementById('check').style.color='black';
-        }
-        else{
-            target.style.borderBottom = "3px solid red";
-            target2.style.borderBottom = "3px solid red";
-            document.getElementById('check').innerHTML='비밀번호가 일치하지 않습니다.';
-            document.getElementById('check').style.color='red';
-        }
-    }
-}
 
 
 /* modal open */
@@ -98,3 +98,5 @@ let num1 = document.querySelector(".num1").addEventListener("change", onChange);
 let num2 = document.querySelector(".num2").addEventListener("change", onChange);
 let num3 = document.querySelector(".num3").addEventListener("change", onChange);
 let num4 = document.querySelector(".num4").addEventListener("change", onChange);
+
+
