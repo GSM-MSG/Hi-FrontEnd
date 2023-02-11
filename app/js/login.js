708a246add9887f53cdbdbb4f967705429ee0020
googoo81 <shw9052@naver.com>
2023-02-12 00:36:30 +0900

    로그인

--- app/js/login.js
+++ app/js/login.js
@@ -10,9 +10,8 @@ function CheckId(str)
     }
 }
 
-function GoToEnroll()
+async function GoToEnroll()
 {
-
 	let obId = document.getElementById("id");
 	if (!obId.value) {
 		alert("아이디을 입력해주십시오.");
@@ -25,5 +24,33 @@ function GoToEnroll()
 			obId.focus();
 			return;
 		}
+		else {
+			console.log(email);
+			console.log(password);
+			fetch("https://port-0-hi-backend-1b5xkk2fldr011vx.gksl2.cloudtype.app/auth/login", {
+                method: 'POST',
+                body: JSON.stringify({
+                    email: email,
+					password: password,
+                }),
+                headers:{
+                    "Access-Control-Allow-Origin": "https://port-0-hi-backend-1b5xkk2fldr011vx.gksl2.cloudtype.app",
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Methods": "GET,PUT,POST,PATCH,DELETE,OPTIONS,REQUEST",
+                    "Access-Control-Allow-Headers": "Content-Type, Authorization, Content-Length, X-Requested-With, Accept",
+                    // "Access-Control-Allow-Credentials": "true",
+                },
+                credentials: 'include'
+            })
+		}
 	}
+}
+
+let email, id, password;
+function inputId () { 
+	id = document.getElementById('id').value;
+	email = id + "gsm.hs.kr"
+}
+function inputPassword () {
+	password = document.getElementById('password').value;
 }
